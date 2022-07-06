7c25bc23a82ad2da3f742c17a4d8eb2acc27b80c
vishangi <vishangid@gmail.com>
2022-07-06 12:39:18 +0530

    Added : Welcome message to Address Book System

---
+++ Contact.java
@@ -0,0 +1,95 @@
+package com.bridgelabz;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long phoneNumber;
+    private String email;
+
+    Contact(){
+        this.setFirstName(this.firstName);
+        this.setLastName(this.lastName);;
+        this.setAddress(this.address);
+        this.setCity(this.city);
+        this.setState(this.state);
+        this.setZip(this.zip);
+        this.setPhoneNumber(this.phoneNumber);
+        this.setEmail(this.email);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    @Override
+    public String toString() {
+        return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
+                + city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email
+                + "]";
+    }
+}
+
